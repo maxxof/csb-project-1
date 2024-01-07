44f83ddff421db119702728a186060505a1dc787
maxxof <maximegorovhy@gmail.com>
2024-01-07 15:14:38 +0200

    add CSRF and broken access control vulns

--- server/pages/views.py
+++ server/pages/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from .models import Account
 from random import randint
-from django.db import connection
+from django.db import connection, transaction
 
 
 # @login_required
@@ -23,30 +23,29 @@ from django.db import connection
 # 		print('not granted')
 # 	return redirect('/')
 	
-@login_required
+# @login_required
 def transferView(request):
-	if request.method == 'POST':
-		request.session['to'] = request.POST.get('to')
-		request.session['amount'] = request.POST.get('amount')
-		amount = request.session['amount']
-		receiver = User.objects.get(username=request.session['to'])
-		sender = request.user.account
+	# if request.method == 'GET':
+	request.session['from'] = request.GET.get('from')
+	request.session['to'] = request.GET.get('to')
+	request.session['amount'] = request.GET.get('amount')
+	amount = request.session['amount']
+	sender = User.objects.get(username=request.session['from'])
+	receiver = User.objects.get(username=request.session['to'])
 
-		try:
-			amount = int(request.session['amount'])
-			sender.balance -= amount
-			amount = receiver.account.balance + amount
-			receiver.account.balance = amount
-		except:
-			pass
+	try:
+		amount = int(request.session['amount'])
+		sender.account.balance -= amount
+		sender.account.save()
+		amount = receiver.account.balance + amount
+	except:
+		pass
 
-		query = f"UPDATE pages_account SET balance = {amount} WHERE user_id = {receiver.id};"
-		print(query)
-		with connection.cursor() as cursor:
-			cursor.execute(query)
-		sender.save()
-		receiver.account.save()
-		# return render(request, 'pages/confirm.html')
+	query = f"UPDATE pages_account SET balance = {amount} WHERE user_id = {receiver.id};"
+	with connection.cursor() as cursor:
+		cursor.execute(query)
+	# receiver.account.save()
+	# return render(request, 'pages/confirm.html')
 	return redirect('/')
 
 
@@ -54,7 +53,7 @@ def transferView(request):
 
 @login_required
 def homePageView(request):
-	request.session['sessionid'] = randint(0, 10)
+	request.session['sessionid'] = randint(1, 2)
 	accounts = Account.objects.exclude(user_id=request.user.id)
 	return render(request, 'pages/index.html', {'accounts': accounts,
 											 'sessionid': request.session['sessionid']})
